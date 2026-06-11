4c8e24cd2b36407ac32d7a8b93ead02c6b46ca42
teodora-mikic <tm20220196@student.fon.bg.ac.rs>
2026-06-11 15:08:23 +0200

    feat: added login and register form and synced with back

---
+++ mobile/app/login.tsx
@@ -0,0 +1,99 @@
+import AuthForm from "@/components/AuthForm";
+import TicketIcon from "@/components/svgs/TicketIcon";
+import TravelIcon from "@/components/svgs/TravelIcon";
+import { API_URL } from "@/constants/api";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useRouter } from "expo-router";
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function LoginScreen() {
+  const router = useRouter();
+  const [error, setError] = useState("");
+
+  const handleLogin = async (data: any) => {
+    try {
+      const response = await fetch(`${API_URL}/api/auth/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        setError("Pogrešni kredencijali");
+        return;
+      }
+
+      const result = await response.json();
+      await AsyncStorage.setItem("token", result.token);
+      router.replace("/");
+    } catch (e) {
+      setError("Greska pri povezivanju sa serverom");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.container1}>
+        <Text style={styles.title}>Dobrodošli</Text>
+        <Text style={styles.paragraph}>
+          Prijavite se da bi rezervisali karte
+        </Text>
+        <TicketIcon
+          size={100}
+          style={{ position: "absolute", top: 40, right: 150 }}
+        />
+      </View>
+      <View style={styles.container2}>
+        <Text style={styles.titleLogin}>Prijava</Text>
+        <AuthForm type="login" onSubmit={handleLogin} error={error} />
+        <TouchableOpacity onPress={() => router.push("/register")}>
+          <Text style={styles.link}>Nemaš nalog? Registruj se</Text>
+        </TouchableOpacity>
+        <TravelIcon
+          size={180}
+          style={{ position: "absolute", top: 420, right: 115 }}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#03757f",
+  },
+  container1: {
+    marginLeft: 90,
+    flex: 0.3,
+    backgroundColor: "#03757f",
+  },
+  container2: {
+    flex: 0.7,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    backgroundColor: "#f0f2f1",
+  },
+  title: {
+    marginTop: 150,
+    fontSize: 43,
+    fontWeight: "bold",
+    color: "#fff",
+    letterSpacing: 2,
+  },
+  link: { textAlign: "center", marginTop: 16, color: "#03757f" },
+  paragraph: {
+    fontSize: 15,
+    color: "#fff",
+    marginLeft: 15,
+  },
+  titleLogin: {
+    marginTop: 40,
+    marginLeft: 30,
+    fontSize: 38,
+    fontWeight: "bold",
+    color: "#03757f",
+    letterSpacing: 2,
+  },
+});
---
+++ mobile/components/AuthForm.tsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+interface AuthFormProps {
+  type: "login" | "register";
+  onSubmit: (data: any) => void;
+  error?: string;
+}
+
+export default function AuthForm({ type, onSubmit, error }: AuthFormProps) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [ime, setIme] = useState("");
+  const [prezime, setPrezime] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = () => {
+    if (type === "login") {
+      onSubmit({ username, password });
+    } else {
+      onSubmit({ username, password, ime, prezime, email });
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      {type === "register" && (
+        <View>
+          <Text style={styles.label}>Ime</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Ime"
+            value={ime}
+            onChangeText={setIme}
+          />
+          <Text style={styles.label}>Prezime</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Prezime"
+            value={prezime}
+            onChangeText={setPrezime}
+          />
+          <Text style={styles.label}>Email adresa</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+          />
+        </View>
+      )}
+      <Text style={styles.label}>Korisničko ime</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <Text style={styles.label}>Lozinka</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      {error && <Text style={styles.error}>{error}</Text>}
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>
+          {type === "login" ? "Prijavi se" : "Registruj se"}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { padding: 20 },
+  input: {
+    borderRadius: 20,
+    padding: 12,
+    backgroundColor: "#ffffff",
+    color: "#8f9191",
+    marginBottom: 13,
+  },
+  button: {
+    backgroundColor: "#03757f",
+    borderRadius: 20,
+    padding: 14,
+    alignItems: "center",
+    marginTop: 15,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 15,
+    letterSpacing: 2,
+  },
+  error: { color: "red", fontSize: 13 },
+  label: {
+    color: "#03757f",
+    fontSize: 15,
+    marginBottom: 5,
+    fontWeight: "600",
+    marginLeft: 8,
+  },
+});

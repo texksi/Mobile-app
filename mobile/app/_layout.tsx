046447f1ae57da8b22689dbfba30620b8ad785d4
teodora-mikic <tm20220196@student.fon.bg.ac.rs>
2026-06-11 12:56:38 +0200

    feat: added login and register form and synced with backend

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,22 +1,35 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import {
+  SourceSans3_400Regular,
+  SourceSans3_700Bold,
+  useFonts,
+} from "@expo-google-fonts/source-sans-3";
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const [fontsLoaded] = useFonts({
+    SourceSans3_400Regular,
+    SourceSans3_700Bold,
+  });
+
+  if (!fontsLoaded) return null;
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+        <Stack.Screen name="register" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="modal"
+          options={{ presentation: "modal", title: "Modal" }}
+        />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
---
+++ mobile/app/register.tsx
@@ -0,0 +1,113 @@
+import AuthForm from "@/components/AuthForm";
+import BusIcon from "@/components/svgs/BusIcon";
+import { API_URL } from "@/constants/api";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function RegisterScreen() {
+  const router = useRouter();
+  const [error, setError] = useState("");
+
+  const handleRegister = async (data: any) => {
+    try {
+      const response = await fetch(`${API_URL}/api/auth/register`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        setError("Greška pri registraciji");
+        return;
+      }
+
+      const result = await response.json();
+      await AsyncStorage.setItem("token", result.token);
+      router.replace("/");
+    } catch (e) {
+      setError("Greška pri povezivanju sa serverom");
+    }
+  };
+
+  return (
+    <View style={styles.containerParent}>
+      <BusIcon
+        size={150}
+        style={{
+          position: "absolute",
+          top: 30,
+          right: -30,
+          zIndex: 10,
+          transform: [{ rotate: "0deg" }, { scaleX: -1 }],
+        }}
+      />
+      <BusIcon
+        size={150}
+        style={{
+          position: "absolute",
+          top: -20,
+          left: -30,
+          zIndex: 10,
+          transform: [{ rotate: "20deg" }],
+        }}
+      />
+      <View style={styles.container1}></View>
+      <View style={styles.container2}>
+        <TouchableOpacity onPress={() => router.push("/login")}>
+          <Text style={styles.arrow}>← Vrati se na prijavu</Text>
+        </TouchableOpacity>
+        <Text style={styles.title}>Registracija</Text>
+        <Text style={styles.paragraph}>Doborodošli na BusTicket</Text>
+        <AuthForm type="register" onSubmit={handleRegister} error={error} />
+        <TouchableOpacity onPress={() => router.push("/login")}>
+          <Text style={styles.link}>Već imaš nalog? Prijavi se</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  containerParent: {
+    flex: 1,
+    backgroundColor: "#03757f",
+  },
+  container1: {
+    flex: 0.2,
+    justifyContent: "center",
+    backgroundColor: "#03757f",
+  },
+  container2: {
+    flex: 0.8,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    backgroundColor: "#f0f2f1",
+  },
+  title: {
+    marginLeft: 20,
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: "left",
+    color: "#03757f",
+    letterSpacing: 3,
+    marginBottom: 5,
+  },
+  paragraph: {
+    marginLeft: 20,
+    fontSize: 18,
+    fontWeight: "400",
+    textAlign: "left",
+    color: "#03757f",
+    letterSpacing: 1,
+  },
+  link: { textAlign: "center", marginTop: 16, color: "#04757d" },
+  arrow: {
+    marginTop: 20,
+    fontSize: 15,
+    marginLeft: 20,
+    marginBottom: 10,
+    color: "#03757f",
+  },
+});

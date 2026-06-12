6ed2ae2459c65205662a763d1e746b92f5516903
teodora-mikic <tm20220196@student.fon.bg.ac.rs>
2026-06-12 14:17:42 +0200

    feat: added new func and edited cors to allow ngrok

--- backend/src/main/java/com/mobileapp/security/SecurityConfig.java
+++ backend/src/main/java/com/mobileapp/security/SecurityConfig.java
@@ -66,8 +66,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8081"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:8081",
+                "https://reexamine-neurotic-dallying.ngrok-free.dev"
+        ));        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
